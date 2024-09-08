644bbfab18d8bc31dcaf22c1458382ecd38eb963
karthick <102713918+karthickag18@users.noreply.github.com>
2024-09-08 08:13:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
